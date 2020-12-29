0e87168a8a472954690ebe517d1c033adeea3d39
Alexander F. Rødseth <rodseth@gmail.com>
2020-12-29 23:42:43 +0100

    Add functions for finding the closest points

--- include/points.hpp
+++ include/points.hpp
@@ -22,3 +22,41 @@ inline std::ostream& operator<<(std::ostream& os, const Points& points)
     }
     return os;
 }
+
+// Find the index to the Vec3 in a std::vector<Vec3> that is
+// closest to the given point p.
+size_t index_closest(const Points xs, const Vec3 p)
+{
+    size_t smallest_i = 0; // return the first point index, by default
+    double smallest_squared_dist = std::numeric_limits<double>::max(); // Largest possible value
+    // TODO: Cache the results from distance_squared
+    for (size_t i = 0; i < xs.size(); ++i) {
+        if (p.distance_squared(xs[i]) < smallest_squared_dist) {
+            smallest_squared_dist = p.distance_squared(xs[i]);
+            smallest_i = i;
+        }
+    }
+    return smallest_i;
+}
+
+// Find the index to the Vec3 in a std::vector<Vec3> that is
+// closest to the given point p, except the given indices.
+size_t index_closest_except(
+    const Points xs, const Vec3 p, const std::vector<size_t> except_indices)
+{
+    int smallest_i = 0; // return the first point index, by default
+    double smallest_squared_dist = std::numeric_limits<double>::max(); // Largest possible value
+    // TODO: Cache the results from distance_squared
+    for (size_t i = 0; i < xs.size(); ++i) {
+        // Thanks Zac Howland at https://stackoverflow.com/a/19299611/131264
+        if (std::any_of(std::begin(except_indices), std::end(except_indices),
+                [&](size_t i2) { return i2 == i; })) {
+            continue;
+        }
+        if (p.distance_squared(xs[i]) < smallest_squared_dist) {
+            smallest_squared_dist = p.distance_squared(xs[i]);
+            smallest_i = i;
+        }
+    }
+    return smallest_i;
+}
